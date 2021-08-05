0870d76cccc66643cd496bcee21f54d28afc3270
Ashwin Kumar V <63821825+ashwin@users.noreply.github.com>
2021-08-05 22:09:25 +0530

    task completed

--- js/script1.js
+++ js/script1.js
@@ -1,9 +1,8 @@
+//Menu icon - Nav links toggling
 function menu(){
-    var a = window.screen.availWidth;
-
-    if (a <= 620){
+    if (window.screen.availWidth <= 620){
         document.getElementById('navlinks').style.display="none";
-        document.getElementById('menu').style.display="block";
+        document.getElementById('menu').style.display="flex";
     }
     else{
         document.getElementById('menu').style.display="none";
@@ -15,3 +14,8 @@ function menu(){
 window.addEventListener('resize', function(){
     menu()
 });
+
+//Menu Icon Onclick Action
+function drop(){
+    document.getElementById('menu').classList.toggle('menu-drop');
+}
