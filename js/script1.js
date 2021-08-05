eddaf5ec59f56e5c6f6fc9da2c08e56cba44e9a3
Ashwin Kumar V <63821825+ashwin@users.noreply.github.com>
2021-08-05 18:15:06 +0530

    menu icon fully responsive

--- js/script1.js
+++ js/script1.js
@@ -1,11 +1,17 @@
-//Window Resize Listener
-window.addEventListener('resize', function(){
+function menu(){
     var a = window.screen.availWidth;
 
     if (a <= 620){
-        document.getElementById('navlinks').style.visibility="hidden";
+        document.getElementById('navlinks').style.display="none";
+        document.getElementById('menu').style.display="block";
     }
     else{
-        document.getElementById('navlinks').style.visibility="visible";
+        document.getElementById('menu').style.display="none";
+        document.getElementById('navlinks').style.display="flex";
     }
+}
+
+//Window Resize Listener
+window.addEventListener('resize', function(){
+    menu()
 });
